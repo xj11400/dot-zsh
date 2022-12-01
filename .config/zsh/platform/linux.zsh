5e0ddca739ca51da2558552355249b47d23718f8
Jase Hsu <Jase_Hsu@wistron.com>
2022-12-01 14:42:21 +0800

    <fix>(platform): wsl display

--- .config/zsh/platform/linux.zsh
+++ .config/zsh/platform/linux.zsh
@@ -3,6 +3,6 @@ if [[ "$MY_DISTRO" == "WSL" ]];then
    alias win="explorer.exe ."
    
    # X.Org server
-   export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
-   export LIBGL_ALWAYS_INDIRECT=1
+   #export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+   #export LIBGL_ALWAYS_INDIRECT=1
 fi
