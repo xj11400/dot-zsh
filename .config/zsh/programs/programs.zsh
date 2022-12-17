52766c5fe737ce278edfceec2e80e2634593f606
XJ Hsu <xj11400@gmail.com>
2022-12-18 05:57:41 +0800

    <feat>(programs): XDG path

--- .config/zsh/programs/programs.zsh
+++ .config/zsh/programs/programs.zsh
@@ -14,13 +14,24 @@ alias nnn='nnn -H'
 
 # vim
 export VIMINIT='source $MYVIMRC'
-export MYVIMRC='$HOME/.config/vim/vimrc'
+export MYVIMRC='$XDG_CONFIG_HOME/vim/vimrc'
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# less
+export LESSHISTFILE="$XDG_CACHE_HOME/less/lesshst"
+
+# wget
+#export WGETRC="$XDG_CONFIG_HOME/wget/wgetrc"
+alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
+
+# python
+export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/python_startup.py"
+export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
+export PYTHONUSERBASE="$XDG_DATA_HOME/python"
 
 # config
 # ======
