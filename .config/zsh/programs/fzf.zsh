40c0398dafe32014fd706b26c65cad269a62e954
XJ Hsu <xj11400@gmail.com>
2023-03-26 15:54:58 +0800

    <modify>(fzf): check command before add path

--- .config/zsh/programs/fzf.zsh
+++ .config/zsh/programs/fzf.zsh
@@ -1,25 +1,31 @@
+declare FZF_PATH=$XDG_DATA_HOME/fzf
+
 # download fzf
 # ------------
 if [[ ! -d $XDG_DATA_HOME/fzf ]];then
     #download fzf
-    x_use "junegunn/fzf" $XDG_DATA_HOME/fzf
+    x_use "junegunn/fzf" $FZF_PATH
     #install fzf
-    bash -c "$XDG_DATA_HOME/fzf/install --xdg --key-bindings --completion --no-update-rc --bin --no-bash --no-zsh --no-fish"
+    bash -c "$FZF_PATH/install --xdg --key-bindings --completion --no-update-rc --bin --no-bash --no-zsh --no-fish"
 fi
+
 
 # setup fzf
 # ---------
-if [[ ! "$PATH" == *$XDG_DATA_HOME/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$XDG_DATA_HOME/fzf/bin"
+if [[ -z $(command -v fzf) ]];then
+    if [[ ! "$PATH" == *$FZF_PATH/bin* ]]; then
+      export PATH="${PATH:+${PATH}:}$FZF_PATH/bin"
+    fi
 fi
+
 
 # auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$XDG_DATA_HOME/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$FZF_PATH/shell/completion.zsh" 2> /dev/null
 
 # key bindings
 # ------------
-source "$XDG_DATA_HOME/fzf/shell/key-bindings.zsh"
+source "$FZF_PATH/shell/key-bindings.zsh"
 
 # show dot files
 # --------------
