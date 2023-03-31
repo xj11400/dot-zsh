ec14acfbeb5a0d46fb98159fc6702cbed61ef02a
XJ Hsu <xj11400@gmail.com>
2023-04-01 05:13:22 +0800

    <feat>(programs): environment variable

---
+++ .config/zsh/programs/lang.zsh
@@ -0,0 +1,18 @@
+#
+# language
+# ############################################################
+
+# python
+# ======
+export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/python_startup.py"
+export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
+export PYTHONUSERBASE="$XDG_DATA_HOME/python"
+
+# go
+# ==
+export GOPATH="$XDG_DATA_HOME"/go
+export GOMODCACHE="$XDG_CACHE_HOME"/go/mod
+
+# rust
+# ====
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
--- .config/zsh/programs/programs.zsh
+++ .config/zsh/programs/programs.zsh
@@ -16,22 +16,12 @@ alias nnn='nnn -H'
 export MYVIMRC='$XDG_CONFIG_HOME/vim/vimrc'
 export VIMINIT='let $MYVIMRC=!has("nvim")?"$XDG_CONFIG_HOME/vim/vimrc":"$XDG_CONFIG_HOME/nvim/init.lua" | so $MYVIMRC'
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # less
 export LESSHISTFILE="$XDG_CACHE_HOME/less/lesshst"
 
 # wget
 #export WGETRC="$XDG_CONFIG_HOME/wget/wgetrc"
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
-
-# python
-export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/python_startup.py"
-export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
-export PYTHONUSERBASE="$XDG_DATA_HOME/python"
 
 # config
 # ======
