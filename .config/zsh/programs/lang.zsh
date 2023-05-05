dec94655dd07ecf16444539abe99cf8b56b27287
XJ Hsu <xj11400@gmail.com>
2023-05-05 09:48:28 +0800

    <feat>(programs): lang add python local path

--- .config/zsh/programs/lang.zsh
+++ .config/zsh/programs/lang.zsh
@@ -4,6 +4,7 @@
 
 # python
 # ======
+PATH=$XDG_DATA_HOME/python/bin:$PATH
 export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/python_startup.py"
 export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
 export PYTHONUSERBASE="$XDG_DATA_HOME/python"
