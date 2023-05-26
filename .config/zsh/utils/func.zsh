b2f036315701c859e8bc7cea05998330c7f15944
XJ Hsu <xj11400@gmail.com>
2023-05-26 11:08:30 +0800

    <fix>(utils): sed on macos

--- .config/zsh/utils/func.zsh
+++ .config/zsh/utils/func.zsh
@@ -3,5 +3,9 @@
 # repalce
 # =======
 x_replace() {
-    sed -i "s+$1+$2+g" `grep -rl $1 $3`
+    if [[ "$MY_OS"=="Darwin" ]];then
+        sed -i "" "s+$1+$2+g" `grep -rl $1 $3`
+    else
+        sed -i "s+$1+$2+g" `grep -rl $1 $3`
+    fi
 }
