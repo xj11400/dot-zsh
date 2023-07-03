949dedea9488f9f10b8717ca0b4273e49e998e4f
XJ Hsu <xj11400@gmail.com>
2023-07-03 14:36:21 +0800

    <modify>(programs): node

--- .config/zsh/programs/node.zsh
+++ .config/zsh/programs/node.zsh
@@ -12,7 +12,7 @@ export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
 
 # nvm
 # ===
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$XDG_CONFIG_HOME/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
