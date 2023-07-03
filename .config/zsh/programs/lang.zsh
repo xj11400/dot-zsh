68a6b2b66c93fb784e606109fe60bf3475d852f0
XJ Hsu <xj11400@gmail.com>
2023-07-03 14:36:34 +0800

    <modify>(program): lang python

--- .config/zsh/programs/lang.zsh
+++ .config/zsh/programs/lang.zsh
@@ -10,6 +10,12 @@ export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
 export PYTHONUSERBASE="$XDG_DATA_HOME/python"
 export PYTHONHISTFILE="$XDG_CACHE_HOME/python/python_history"
 
+if [ "$(command -v pyenv)" ]; then
+    export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+fi
+
 # go
 # ==
 export GOPATH="$XDG_DATA_HOME"/go
