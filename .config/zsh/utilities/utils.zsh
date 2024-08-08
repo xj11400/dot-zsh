60a62814ce30344d262883cf3720f7b1b4777067
XJ Hsu <xj11400@gmail.com>
2024-08-08 14:46:17 +0800

    <feat>(utilies/utils): _delete_files function

--- .config/zsh/utilities/utils.zsh
+++ .config/zsh/utilities/utils.zsh
@@ -12,3 +12,18 @@ x_replace() {
         sed -i "s+$1+$2+g" `grep -rl $1 $3`
     fi
 }
+
+# Function to delete all files recursively under the given directory
+# Usage: delete_files "file_name" <directory>
+_delete_files() {
+    local file_name="$1"
+    local dir="${2:-.}"  # Use current directory if no directory argument is provided
+    if [ -d "$dir" ]; then
+        find "$dir" -type f -name "$file_name" -print -delete | while read -r file; do
+            echo " > Deleted: $file"
+        done
+        echo "Finished deleting $file_name files under $dir"
+    else
+        echo "Error: $dir is not a valid directory"
+    fi
+}
