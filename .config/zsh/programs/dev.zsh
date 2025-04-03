cc0e5631a3a110e35efac364ba660f5ff2c61c0c
XJ Hsu <xj11400@gmail.com>
2025-04-03 16:13:04 +0800

    <feat>(programs/dev): dotenv

--- .config/zsh/programs/dev.zsh
+++ .config/zsh/programs/dev.zsh
@@ -10,3 +10,9 @@ export MACHINE_STORAGE_PATH="$XDG_DATA_HOME"/docker-machine
 # minikube
 # ========
 export MINIKUBE_HOME="$XDG_DATA_HOME"/minikube
+# dotenv
+# ======
+if [ "$(command -v direnv)" ]; then
+    eval "$(direnv hook zsh)"
+fi
+
