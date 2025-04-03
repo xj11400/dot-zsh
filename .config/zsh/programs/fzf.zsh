b8ed3132c51294e970075c9707a856215a6b587a
XJ Hsu <xj11400@gmail.com>
2025-04-03 16:13:22 +0800

    <feat>(programs/fzf): default opts

--- .config/zsh/programs/fzf.zsh
+++ .config/zsh/programs/fzf.zsh
@@ -29,4 +29,26 @@ source "$FZF_PATH/shell/key-bindings.zsh"
 
 # show dot files
 # --------------
-export FZF_DEFAULT_COMMAND='ag -l --path-to-ignore ~/.ignore --nocolor --hidden -g ""'
+
+# Default options
+export FZF_DEFAULT_OPTS='--bind=ctrl-j:accept,ctrl-k:kill-line'
+
+# Default command to use when input is tty
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+
+# CTRL-T - Paste the selected files and directories onto the command-line
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# export FZF_CTRL_T_OPTS="--preview 'bat --color=always -n --line-range :500 {}'"
+
+# ALT-C - cd into the selected directory
+# export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+# export FZF_ALT_C_OPTS="--preview 'eza --icons=always --tree --color=always {} | head -200'"
+
+# CTRL-R - Paste the selected command from history onto the command-line
+# export FZF_CTRL_R_OPTS="
+#   --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+#   --color header:italic
+#   --header 'Press CTRL-Y to copy command into clipboard'"
+
+# # fzf preview for tmux
+# export FZF_TMUX_OPTS=" -p90%,70% "
