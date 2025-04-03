01cd6df338e40ee63abe0cfa9a9dd82fe77897b4
XJ Hsu <xj11400@gmail.com>
2025-04-03 16:13:55 +0800

    <feat>(programs/programs): yazi

--- .config/zsh/programs/programs.zsh
+++ .config/zsh/programs/programs.zsh
@@ -5,11 +5,21 @@
 # alias
 # =====
 alias ra='$(command -v ranger)'
-alias jo='$(command -v joshuto)'
 alias lgit='lazygit'
 alias ldocker='lazydocker'
-alias nnn='nnn -H'
-alias yi='$(command -v yazi)'
+
+# yazi
+if [ "$(command -v yazi)" ]; then
+    alias yi='$(command -v yazi)'
+    function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+            builtin cd -- "$cwd"
+        fi
+        rm -f -- "$tmp"
+    }
+fi
 
 # XDG path
 # ========
